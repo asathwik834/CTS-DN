48acabcf164f0b55e08fc151bb74bcf72ac93df8
AARADHYULA SATHWIK <sathwik@SATHWIKs-MacBook.local>
2026-07-21 22:05:54 +0530

    upskilling updated

--- Upskilling/Module 2/ANSI-SQL-Using-MySQL/Exercise-10/solution.sql
+++
@@ -1,7 +0,0 @@
-SELECT DISTINCT
-    u.user_id,
-    u.full_name,
-    u.email
-FROM Users u
-JOIN Registrations r
-    ON u.user_id = r.user_id;
--- Upskilling/Module 2/ANSI-SQL-Using-MySQL/Exercise-9/solution.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    e.event_id,
-    e.title,
-    COUNT(f.feedback_id) AS feedback_count
-FROM Events e
-LEFT JOIN Feedback f
-    ON e.event_id = f.event_id
-GROUP BY e.event_id, e.title
-ORDER BY feedback_count DESC;
